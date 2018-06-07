816db1b47d44a7a54aa55ef9564e4fceb20b7734
John Starich <johnstarich@johnstarich.com>
2018-06-07 02:22:25 -0500

    Add initial landing page

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    autoprefixer: {
+      browsers: ['last 5 years'],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
